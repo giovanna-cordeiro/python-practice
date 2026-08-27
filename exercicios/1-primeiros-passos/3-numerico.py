c0fa44100a3adfba6edfc638f6bede94832638c0
Giovanna Cordeiro <giovannacordeirorodrigues@gmail.com>
2026-08-27 13:17:58 +0000

    Resolução exercícios numéricos python

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,34 @@
-data_nascimento = '05-10-1976'
-idade_numerica = 46
-altura = 1.74
+data_nascimento = '29-03-2006'
+idade_numerica = 20
+altura = 1.65
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
-
+nome = 'Giovanna'
+nome * 3
+print(nome * 3)
+print((nome + ' ')*3)
 # Realize uma operação entre dados do tipo inteiro e float
+soma = idade_numerica + 2.5
+print(soma)
+print(type(soma))
+
+#Realize uma operação de divisão entre dois números inteiros 
+divisao = 6/3
+print(divisao)
+
+#Realize conversões numéricas 
+decimal = 30/8
+print(decimal)
+
+print (int(decimal))
+
+print (round(decimal))
+
+print(round(decimal,1))
+
+print (round(decimal,0))
