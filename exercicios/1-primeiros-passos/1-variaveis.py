4b487811a850469c62727a6f74521ccdf8dc374d
Giovanna Cordeiro <giovannacordeirorodrigues@gmail.com>
2026-08-27 12:12:28 +0000

    Resolução exercício de Variáveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Giovanna'
+profissao = 'estagiaria'
+objetivo = 'aprender python'
 # Declare variáveis de cada tipo numérico
-
+idade = '20'
+idade_meses = '20.5'
 
 # Declare uma variável do tipo booleano
+verdadeiro = True 
+falso = False
+
+
+
