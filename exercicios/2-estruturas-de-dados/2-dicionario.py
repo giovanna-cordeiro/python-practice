a1aac47a4f898e4994c904b2aa6be8028012f5a5
Giovanna Cordeiro <giovannacordeirorodrigues@gmail.com>
2026-08-28 10:58:12 +0000

    Resolução dicionário

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -1,18 +1,22 @@
-pessoa = {'nome':'Crislaine', 
-          'idade':46, 
-          'ano_formatura':2010, 
-          'linguagens_programacao':['python', 'r', 'javascript', 'ruby'], 
+pessoa = {'nome':'Giovanna', 
+          'idade':20, 
+          'ano_formatura':2027, 
+          'linguagens_programacao':['python', 'Java', 'javascript', 'C', 'C#'], 
           'brasileira':True, 
-          'hobby':['nadar', 'ler', 'pedalar'], 
-          'animal_estimacao':False}
+          'hobby':['Ler', 'correr', 'Ver filmes'], 
+          'animal_estimacao':True}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print(pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
+valores = list(pessoa.values())
+print(valores)
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
+chaves = list(pessoa.keys())
+print(chaves)
 
 # Insira um novo par chave-valor no dicionário
+pessoa['altura'] = 1.65
+print(pessoa['altura'])
