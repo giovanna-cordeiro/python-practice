838d93ec1015fb352e769ef7184f2358ac63c3f0
Giovanna Cordeiro <giovannacordeirorodrigues@gmail.com>
2026-08-31 14:26:11 +0000

    Resolução projeto avaliação de cursos

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,28 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+
+cursos = ['python', 'Java', 'SQL', 'excel', 'PowerBi']
+
+curso_python = 'python'
+curso_java = 'Java'
+curso_sql = 'SQL'
+
+notas_curso = {}
+
+if curso_python in cursos:
+  print(f'Este curso está em nosso catálogo, dê uma nota a esse curso')
+  notas_curso[curso_python] = int(input('Dê uma nota de 0 a 5 para esse curso:'))
+
+if curso_java in cursos:
+  print(f'Este curso está em nosso catálogo')
+notas_curso[curso_java] = int(input('Dê uma nota de 0 a 5 para esse curso:'))
+
+if curso_sql in cursos:
+  print(f'Este curso está em nosso catálogo')  
+  notas_curso[curso_sql] = int(input('Dê uma nota de 0 a 5 para esse curso:'))
+  
+else:
+  print(f'Esse curso não faz parte do nosso catálogo')
+
+print(notas_curso)
